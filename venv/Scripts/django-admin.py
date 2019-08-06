9078e88d0365be93bf43f79f72ace12f1ca6c0b7
Damirkhan <d.aloev@student.inha.uz>
2019-08-06 17:14:33 +0300

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Damirkhon Aloev\Desktop\pythony\Projects\tenderApp\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

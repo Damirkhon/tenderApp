95cd0d3b41a917c6b66ee17e235ce5f9da55add7
Damirkhan <d.aloev@student.inha.uz>
2019-08-26 09:53:03 +0300

    im back

--- tenderApp/settings.py
+++ tenderApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'gtpf_gkrq256y5h&7qa@5&&%ou3mb1u%md5y4q$k!9&ts((gnv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
